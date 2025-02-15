f3a7382fb929ab5b30accd73d9fc59d9f3f71b1f
Isabelle Pinheiro Lovo <isabelle.pinheirolv@gmail.com>
2025-02-15 14:02:40 -0300

    add python files

---
+++ Python/3-periodo/Lista-Trabalho-1/trab1-ex2.py
@@ -0,0 +1,9 @@
+# 2. Solicite a largura e a altura de um retângulo e calcule a área.
+
+print('2. Preencha com os dados de um Retângulo!')
+
+larg = float(input('Largura: '))
+alt = float(input('Altura: '))
+area = larg * alt
+
+print(f'Área do Retângulo = {area}')
---
+++ Python/3-periodo/Lista-Trabalho-1/trab1-ex3.py
@@ -0,0 +1,6 @@
+# 3. Peça dois números inteiros e use o operador de igualdade (==) para verificar se eles são iguais.
+
+a = int(input('Digite um valor inteiro para A: '))
+b = int(input('Digite um valor inteiro para B: '))
+
+print(f'A é igual a B? {a == b}')
